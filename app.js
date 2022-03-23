3f4282cb23eaba7dee6b1f21c00c93c747f3ec1d
Carlos <cajavi89@gmail.com>
2022-03-23 10:51:37 -0500

    implemented swagger documentation

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const expressConfig= require('./config/express')
-
 const routes = require('./routes');
-
+const swaggerDocs = require('./config/swagger')
 const app = express();
+
 
 expressConfig(app);
 
@@ -14,6 +14,9 @@ app.listen(PORT, ()=>{
 
   //routes
   routes(app)
+
+  //swagger
+  swaggerDocs(app, PORT);
 
   //message all running well.
   console.log(`Server Runing at http://localhost:${PORT}`)
--- routes.js
+++ routes.js
@@ -1,7 +1,6 @@
 const users = require('./api/user')
 
 function routes(app){
-  // app.use('/',(req,res)=>{res.send('Welcome to my users Backend')})
   app.use('/api/users', users)
 }
 
